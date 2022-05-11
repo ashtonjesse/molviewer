44546810e12d680e59f9eb7f734950ad5dd901aa
ashtonjesse <ashton.jesse@gmail.com>
2022-05-11 22:51:49 +1200

    full functionality, minor edits required to Macromolecule.save()

--- src/molviewer/main.py
+++ src/molviewer/main.py
@@ -1,4 +1,8 @@
-from molecules import Macromolecule
-
-Macromolecule(pdbid="6LU7")
-
+from molecules import ChemicalMolecule, Macromolecule
+protease = Macromolecule(pdbid="6LU7")
+lopinavir = ChemicalMolecule(chemblid="CHEMBL729")
+viewer = protease.show()
+lopinavir.show(viewer)
+#file_path = protease.save("C:\\Users\\jash042\\Downloads")
+file_path = lopinavir.save("C:\\Users\\jash042\\Downloads\\CHEMBL729.sdf")
+my_test = 1
--- src/molviewer/molecules.py
+++ src/molviewer/molecules.py
@@ -1,14 +1,12 @@
-import requests
 from chembl_webresource_client.new_client import new_client
-from rdkit.Chem import AllChem as Chem
-import Bio as biopython
-import json
-import jupyterlab
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from Bio.PDB import PDBList
 
 from typing import Union, List, Dict
 from nglview.component import ComponentViewer
+import nglview as nv
 from pathlib import Path
-
 
 class Molecule:
     def __init__(self) -> None:
@@ -25,8 +23,19 @@ class Macromolecule(Molecule):
         super().__init__()
         self.pdbid = pdbid
 
+    def show(self, existing_viewer: Union[ComponentViewer, None] = None) -> ComponentViewer:
+        if existing_viewer == None:
+            #existing_viewer = nv.NGLWidget()
+            return nv.show_pdbid(self.pdbid)
+        else:
+            return existing_viewer.add_component(nv.adaptor.PdbIdStructure(self.pdbid))
+
     def save(self, file_path: Union[str, Path]) -> Path:
-        pass
+        pdbl = PDBList()
+        file_out = pdbl.retrieve_pdb_file(pdb_code=self.pdbid, pdir=file_path, file_format='pdb')
+        return Path(file_out)
+
+
 
 class ChemicalMolecule(Molecule):
     """
@@ -42,16 +51,24 @@ class ChemicalMolecule(Molecule):
     def get_compounds_api(self) -> new_client.molecule:
         return new_client.molecule
 
-    def get_mol_data(self, chemblid: str) -> Dict:
-        return self.compounds_api.filter(chembl_id=chemblid).only(['molecule_chembl_id', 'pref_name', 'molecule_structures'])
+    def get_mol_data(self, chemblid: str) -> List[Dict]:
+        return self.compounds_api.filter(chembl_id=chemblid).only(['molecule_chembl_id', 'molecule_structures'])
 
-    def get_conformer(self) -> rdkit.Chem.rdchem.Mol:
-        mol = Chem.AddHs(Chem.MolFromSmiles(self.mol_data['molecular_structures']['canonical_smiles']))
+    def get_conformer(self) -> Chem.rdchem.Mol:
+        mol = Chem.AddHs(Chem.MolFromSmiles(self.mol_data[0]['molecule_structures']['canonical_smiles']))
         _ = AllChem.EmbedMultipleConfs(mol, useExpTorsionAnglePrefs=True, useBasicKnowledge=True)
         return mol
 
     def show(self, existing_viewer: Union[ComponentViewer, None] = None) -> ComponentViewer:
-        return nv.show_rdkit(self.conformer)
+        if existing_viewer == None:
+            #existing_viewer = nv.NGLWidget()
+            return nv.show_rdkit(self.conformer)
+        else:
+            return existing_viewer.add_component(nv.adaptor.RdkitStructure(self.conformer))
 
     def save(self, file_path: Union[str, Path]) -> Path:
-        pass
+        with Chem.SDWriter(file_path) as writer:
+            for cid in range(self.conformer.GetNumConformers()):
+                writer.write(self.conformer, confId=cid)
+
+        return Path(file_path)
