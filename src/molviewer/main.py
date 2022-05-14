aad17701011e6f528af3661a959cd41c592ac019
ashtonjesse <ashton.jesse@gmail.com>
2022-05-15 11:11:41 +1200

    Working on ci-cd workflow

--- src/molviewer/main.py
+++ src/molviewer/main.py
@@ -1,4 +1,4 @@
-from molecules import ChemicalMolecule, Macromolecule # type: ignore
+"""from molecules import ChemicalMolecule, Macromolecule # type: ignore
 
 protease = Macromolecule(pdbid="6LU7")
 lopinavir = ChemicalMolecule(chemblid="CHEMBL729")
@@ -6,4 +6,4 @@ viewer = protease.show()
 lopinavir.show(viewer)
 file_path = protease.save("C:\\Users\\jash042\\Downloads\\6LU7.pdb")
 file_path = lopinavir.save("C:\\Users\\jash042\\Downloa\\CHEMBL729.sdf")
-my_test = 1
+my_test = 1"""
